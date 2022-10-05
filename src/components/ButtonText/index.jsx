a137687cbdfecc9637b260121e00363e4f9feb20
GomidesTs <gomidestulio5@gmail.com>
2022-10-05 19:09:59 -0300

    :sparkles: feat: components responsible for managing the aplication

---
+++ src/components/ButtonText/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from './styles';
+
+
+export function ButtonText({ title, isActive = false, ...res }) {
+    return (
+        <Container
+            type='button'
+            isActive={isActive}
+            {...res}
+        >
+            {title}
+        </Container>
+    )
+}
